57ac720b97fc3291e2005bb7b1a50fb0cc5b3de9
alex <mmerenstein@cs.stonybrook.edu>
2024-05-05 22:39:20 -0500

    Add yamls for running mongo and minio on k8s

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -9,7 +9,7 @@ TOPLEVEL=$(git rev-parse --show-toplevel)
 # TODO: check if tarball exists, 
 # if not tell user to run python setup.py sdist
 cp ${TOPLEVEL}/dist/orchestration-0.1.tar.gz build
-cp ${TOPLEVEL}/config/default.ini build
+cp ${TOPLEVEL}/config/in-pod-config.ini build/config.ini
 
 docker build . -t $TAG
 
